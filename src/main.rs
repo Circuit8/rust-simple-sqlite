1f13a660bed1a7ffb826944925f9b4017267dbcd
Joshua Harris <joshuaweharris@googlemail.com>
2020-06-04 12:33:59 +0100

    Got basic REPL working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use std::io::{BufRead, Stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = io::stdout();
+    print_prompt(&mut stdout);
+
+    for line in io::stdin().lock().lines() {
+        let command = line.unwrap();
+        match command.as_str() {
+            ".exit" => return,
+            _ => println!("Unknown command {:?}", command),
+        }
+
+        print_prompt(&mut stdout);
+    }
+}
+
+fn print_prompt(stdout: &mut Stdout) {
+    print!("db > ");
+    stdout.flush().unwrap();
 }
